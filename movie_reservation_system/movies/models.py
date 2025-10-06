4b378435fcc0f0a4d625fb7093e92f54d9f498ca
MistFantasy <yustyn-danylo.riznyk.pz.2020@lpnu.ua>
2025-10-06 22:44:35 +0300

    - created models (needs to create Images and tests)

--- movie_reservation_system/movies/models.py
+++ movie_reservation_system/movies/models.py
@@ -2,4 +2,23 @@ from django.db import models
 
 
 class Movie(models.Model):
-    pass
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+class MovieGenre(models.Model):
+    headline = models.CharField(max_length=100)
+    movies = models.ManyToManyField(Movie)
+
+    class Meta:
+        ordering = ["headline"]
+
+    def __str__(self):
+        return self.headline
