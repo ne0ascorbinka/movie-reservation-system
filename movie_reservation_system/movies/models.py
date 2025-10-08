67462228d4ba545e81a28a01692dcd3d82003d91
MistFantasy <yustyn-danylo.riznyk.pz.2020@lpnu.ua>
2025-10-09 01:04:18 +0300

    + added image field, fixed many to many, add some tests

--- movie_reservation_system/movies/models.py
+++ movie_reservation_system/movies/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 
 
+class MovieGenre(models.Model):
+    headline = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["headline"]
+
+    def __str__(self):
+        return self.headline
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(
+        upload_to=None,
+        height_field=None,
+        width_field=None,
+        max_length=100,
+        blank=True
+    )
+    genres = models.ManyToManyField(MovieGenre, related_name='movies')
 
     class Meta:
         ordering = ["title"]
@@ -13,12 +31,3 @@ class Movie(models.Model):
     def __str__(self):
         return self.title
 
-class MovieGenre(models.Model):
-    headline = models.CharField(max_length=100)
-    movies = models.ManyToManyField(Movie)
-
-    class Meta:
-        ordering = ["headline"]
-
-    def __str__(self):
-        return self.headline
--- movie_reservation_system/movies/tests.py
+++ movie_reservation_system/movies/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
-
+from movies.models import Movie
+from movies.models import MovieGenre
 # Create your tests here.
+
+class MoviesTest(TestCase):
+    def test_create_mymodel_instance(self):
+        instance = Movie.objects.create(title="Test Name",
+                                        description="Some Description")
+
+        self.assertEqual(Movie.objects.count(), 1)
+        self.assertEqual(instance.title, "Test Name")
+        self.assertEqual(instance.description, "Some Description")
+
+    def test_mymodel_string_representation(self):
+        instance = Movie.objects.create(title="Another Test")
+        self.assertEqual(str(instance), "Another Test")
+
+class MoviesGenreTest(TestCase):
+    def test_create_mymodel_instance(self):
+        instance = MovieGenre.objects.create(headline="Test Name")
+
+        self.assertEqual(MovieGenre.objects.count(), 1)
+        self.assertEqual(instance.headline, "Test Name")
+
+    def test_mymodel_string_representation(self):
+        instance = Movie.objects.create(title="Another Test")
+        self.assertEqual(str(instance), "Another Test")
