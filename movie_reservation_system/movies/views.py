256a2dd1405f23e85767bb7556fa71406d2f7662
ne0ascorbinka <ne.ascorbinka@gmail.com>
2025-10-16 22:13:24 +0300

    feat(movies): add movie list view and URL configuration

--- movie_reservation_system/movies/views.py
+++ movie_reservation_system/movies/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from movies.models import Movie
 # Create your views here.
+class MovieListView(ListView):
+    model = Movie
+    template_name = "movie_list.html"
+
