6742086ac91e89aa03109f8b09716c65aead2ad8
Ighoorr <i.bereshchak@gmail.com>
2025-10-17 20:41:45 +0300

    booking

--- movie_reservation_system/movies/urls.py
+++ movie_reservation_system/movies/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path("day/<slug:date_str>/", views.upcoming_showtimes, name="showtimes_by_date"),
     path("all/", views.movie_list, name="movie_list"),
     path('<int:movie_id>/', views.movie_detail, name='movie_detail'),
+    path("booking/<int:showtime_id>/", views.booking_detail, name="booking_detail"),
+    path("showtime/<int:showtime_id>/success/", views.booking_success, name="booking_success"),
+
 
 
 ]
