615d3947bec2475d5dd08f072b4ea607080a171d
ne0ascorbinka <ne.ascorbinka@gmail.com>
2025-12-01 01:04:26 +0200

    refactor: convert terms and privacy views to class-based TermsView and PrivacyView

--- movie_reservation_system/users/urls.py
+++ movie_reservation_system/users/urls.py
@@ -1,12 +1,12 @@
 from django.views.generic import TemplateView
 from django.urls import path
-from .views import RegisterView, terms, privacy, UserLoginView, UserLogoutView
+from .views import RegisterView, TermsView, PrivacyView, UserLoginView, UserLogoutView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('terms/', terms, name='terms'),
-    path('privacy/', privacy, name='privacy'),
+    path('terms/', TermsView.as_view(), name='terms'),
+    path('privacy/', PrivacyView.as_view(), name='privacy'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
